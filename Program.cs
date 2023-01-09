16c2b823caec014eabd79c8cdbb7ae4dfc3614f2
leotricano <leotricano@gmail.com>
2023-01-09 19:04:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Importando do arquivo de classe
+using bytebank;
+
+// Criando Objeto contaDoAndre
+ContaCorrente contaDoAndre = new ContaCorrente(); 
+
+//Inserindo valores para os objetos
+contaDoAndre.titular = "Andre Silva";
+//Inserindo valores para os objetos
+contaDoAndre.numero_agencia= 15;
+//Inserindo valores para os objetos
+contaDoAndre.conta = "1010-X";
+//Inserindo valores para os objetos
+contaDoAndre.saldo = 100;
+
+//Mostrando o valor atravez do WriteLine
+Console.WriteLine("Saldo conta do Andre = " + contaDoAndre.saldo);
+
---
+++ contaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class ContaCorrente // Definição de Classe "Conta Corrente"
+    {
+       public int numero_agencia; //Campos do objeto
+       public string conta;
+       public string titular;
+       public double saldo;
+    }
+}
