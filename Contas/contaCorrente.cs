d08f77144c25ea7a2e927bb5074f47adc80284e6
leotricano <leotricano@gmail.com>
2023-01-26 21:13:51 -0300

    Namespace

--- Contas/contaCorrente.cs
+++ Contas/contaCorrente.cs
@@ -4,21 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bytebank
+namespace bytebank.Contas
 
 {// Definição de Classe "Conta Corrente"
     public class ContaCorrente
     {  //Campos do objeto
         public int numero_agencia;
         public string conta;
-        public string titular;
         public double saldo;
+
+        public Cliente titular; //  Com isso estamos criando uma referência. Esse campo vai receber, na verdade,
+                                //  uma referência para um objeto do tipo Cliente.
+                                //  Assim, relacionamos as duas classes através desse campo.
+
 
         // Criando metodo Depositar
         public void Depositar(double valor)
-        {   
+        {
             //Definindo o comportamento do metodo
-            this.saldo += valor;
+            saldo += valor;
         }
         // Criando metodo Saque
         public bool Saque(double valor)
@@ -26,27 +30,28 @@ namespace bytebank
             //Uma validação para ser possivel sacar a quantia real do dinheiro depositado,
             //se o valor for menor ou igual ao saldo ele ira fazer o deposito
             //Se não retorna false
-            if (valor <= this.saldo)
+            if (valor <= saldo)
             {
                 //Definindo o comportamento do metodo
-                this.saldo -= valor;
+                saldo -= valor;
                 return true;
             }
-            else 
+            else
             {
                 return false;
             }
         }
-        public bool Transferir(double valor, ContaCorrente destino)
+        //Criando metodo transferir
+        public bool Transferir(double valor, ContaCorrente destino) //declarando o valor e qual conta corrente utilizar, apelidando de "destino"
         {
-            if(this.saldo < valor)
+            if (saldo < valor) // Se o saldo for menor do que o valor a ser depositado não acontece nada, pois ira ficar negativo
             {
-                return false;
+                return false; // Acontece nada se acontecer o If
             }
             else
             {
-                this.saldo -= valor;
-                destino.saldo += valor;
+                saldo -= valor;    // Valor ira retirar o dinheiro do saldo (ex: saldo 500 - Valor 300 = Saldo 200)
+                destino.saldo += valor; // Ira somar ao destinatario no caso seria uma soma
 
                 return true;
             }
--- Program.cs
+++ Program.cs
@@ -1,68 +1,85 @@
-﻿//Importando do arquivo de classe
+﻿////Importando do arquivo de classe
 using bytebank;
 
 
 
-// Criando Objeto contaDoAndre
-ContaCorrente contaDoAndre = new ContaCorrente(); 
+//// Criando Objeto contaDoAndre
+//ContaCorrente contaDoAndre = new ContaCorrente();
 
-//Inserindo valores para os objetos
-contaDoAndre.titular = "Andre Silva";
-//Inserindo valores para os objetos
-contaDoAndre.numero_agencia= 15;
-//Inserindo valores para os objetos
-contaDoAndre.conta = "1010-X";
-//Inserindo valores para os objetos
-contaDoAndre.saldo = 100;
+////Inserindo valores para os objetos
+//contaDoAndre.titular = "Andre Silva";
+////Inserindo valores para os objetos
+//contaDoAndre.numero_agencia = 15;
+////Inserindo valores para os objetos
+//contaDoAndre.conta = "1010-X";
+////Inserindo valores para os objetos
+//contaDoAndre.saldo = 100;
 
-//Mostrando o valor atravez do WriteLine
-Console.WriteLine("Saldo conta do Andre = " + contaDoAndre.saldo);
+////Mostrando o valor atravez do WriteLine
+//Console.WriteLine("Saldo conta do Andre = " + contaDoAndre.saldo);
 
-//Utlizando o metodo Depositar
-contaDoAndre.Depositar(100);
+////Utlizando o metodo Depositar
+//contaDoAndre.Depositar(100);
 
-//Mostrando resultado depois de ter usado o metodo Depositar
-Console.WriteLine("Saldo conta do Andre pós-deposito = " + contaDoAndre.saldo);
+////Mostrando resultado depois de ter usado o metodo Depositar
+//Console.WriteLine("Saldo conta do Andre pós-deposito = " + contaDoAndre.saldo);
 
-//Utilizando o metodo Saque
-if(contaDoAndre.Saque(400))
-{   //Mostrando resultado depois de ter usado o metodo Saque
-    Console.WriteLine("Saldo conta do Andre pós-saque = " + contaDoAndre.saldo);
-}
-else
-{   //Mostrando resultado depois de ter usado o metodo Saque
-    Console.WriteLine("Saldo inuficiente para saque");
-}
-
-
+////Utilizando o metodo Saque
+//if(contaDoAndre.Saque(400))
+//{   //Mostrando resultado depois de ter usado o metodo Saque
+//    Console.WriteLine("Saldo conta do Andre pós-saque = " + contaDoAndre.saldo);
+//}
+//else
+//{   //Mostrando resultado depois de ter usado o metodo Saque
+//    Console.WriteLine("Saldo inuficiente para saque");
+//}
 
 
-// Criando Objeto contaDaMaria
-ContaCorrente contaDaMaria = new ContaCorrente();
 
-//Inserindo valores para os objetos
-contaDaMaria.titular = "Maria Souza";
-//Inserindo valores para os objetos
-contaDaMaria.numero_agencia = 17;
-//Inserindo valores para os objetos
-contaDaMaria.conta = "1010-5";
-//Inserindo valores para os objetos
-contaDaMaria.saldo = 350;
-//Mostrando o saldo da Maria no programa
-Console.WriteLine("Saldo da conta da Maria =" + contaDaMaria.saldo);
 
-//Utilizando o metodo Transferir 
-contaDaMaria.Transferir(50, contaDaMaria);
-Console.WriteLine("Saldo do Andre = " + contaDoAndre.saldo);
-Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
-/*
+//// Criando Objeto contaDaMaria
+//ContaCorrente contaDaMaria = new ContaCorrente();
+
+////Inserindo valores para os objetos
+//contaDaMaria.titular = "Maria Souza";
+////Inserindo valores para os objetos
+//contaDaMaria.numero_agencia = 17;
+////Inserindo valores para os objetos
+//contaDaMaria.conta = "1010-5";
+////Inserindo valores para os objetos
+//contaDaMaria.saldo = 350;
+////Mostrando o saldo da Maria no programa
+//Console.WriteLine("Saldo da conta da Maria =" + contaDaMaria.saldo);
+
+////Utilizando o metodo Transferir 
+//contaDoAndre.Transferir(100, contaDaMaria);
+//Console.WriteLine("Saldo do Andre = " + contaDoAndre.saldo);
+//Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+
+//ContaCorrente conta = new ContaCorrente();
+//Console.WriteLine($"Titular da conta: {conta.titular}");
+//Console.WriteLine($"Número da conta: {conta.conta}");
+//Console.WriteLine($"Agência: {conta.numero_agencia}");
+//Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta.saldo)}");
+
+//Console.ReadKey();
+
+//*/
+
+Cliente cliente = new Cliente();
+cliente.nome = "Andre Silva";
+cliente.cpf = "12345678";
+cliente.profissao = "Analista";
 
 ContaCorrente conta = new ContaCorrente();
-Console.WriteLine($"Titular da conta: {conta.titular}");
-Console.WriteLine($"Número da conta: {conta.conta}");
-Console.WriteLine($"Agência: {conta.numero_agencia}");
-Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta.saldo)}");
+conta.titular = cliente;
+conta.conta = "1010-X";
+conta.numero_agencia = 15;
+conta.saldo = 100;
 
-Console.ReadKey();
-
-*/
+Console.WriteLine("Titular = "+conta.titular.nome);
+Console.WriteLine("CPF = " + conta.titular.cpf);
+Console.WriteLine("Profissão = " + conta.titular.profissao);
+Console.WriteLine("N Conta = " + conta.conta);
+Console.WriteLine("Saldo = " + conta.saldo);
+Console.WriteLine("N Agência = " + conta.numero_agencia);
